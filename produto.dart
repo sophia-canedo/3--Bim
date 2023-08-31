668c027a85a20d5ee4117672acd9915742a9b1e1
sophia-canedo <130404470+sophia-canedo@users.noreply.github.com>
2023-08-31 09:24:49 -0300

    Atividade 24/08

---
+++ produto.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Produto {
+  final String id;
+  final List<String> categories;
+  final String title;
+  final double cost;
+  final String imageUrl;
+  final double duration;
+  final bool isGlutenFree;
+  final bool isVegan;
+  final bool isVegetarian;
+  final bool isLactoseFree;
+
+  const Produto({
+    required this.id,
+    required this.categories,
+    required this.title,
+    required this.cost,
+    required this.imageUrl,
+    required this.duration,
+    required this.isGlutenFree,
+    required this.isVegan,
+    required this.isVegetarian,
+    required this.isLactoseFree,
+  });
+}
+
